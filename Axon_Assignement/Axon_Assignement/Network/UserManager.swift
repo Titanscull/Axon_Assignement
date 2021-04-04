7928fa310e01ec2a944a6447a899b61ef610ca2c
Titanscull <ispetrovskyi@gmail.com>
2021-04-04 17:05:50 +0300

    User Manager Set up

--- Axon_Assignement/Axon_Assignement/Network/UserManager.swift
+++ Axon_Assignement/Axon_Assignement/Network/UserManager.swift
@@ -7,3 +7,63 @@
 
 import Foundation
 
+protocol UserEndPointProtocol: class {
+    func fetchUsers(completion: @escaping ((Result<[User], NetworkError>) -> Void))
+}
+
+enum NetworkError: Error {
+    case urlIsNotValid
+    case sessionIsNotValid
+    case brokenResponse
+    case NoDataRecieved
+    case unableToDecode
+    case unexpectedStatusCode(Int)
+}
+
+class UserManager: UserEndPointProtocol {
+    
+    let usersNumber = 20
+    
+    private lazy var url = "https://randomuser.me/api/?results=\(usersNumber)"
+    
+    let session = URLSession.shared
+    
+    func fetchUsers(completion: @escaping ((Result<[User], NetworkError>) -> Void)) {
+        
+        guard let url = URL(string: url) else {
+            completion(.failure(.urlIsNotValid))
+            return
+        }
+        
+        session.dataTask(with: url) { data, response, error in
+            
+            if error != nil {
+                completion(.failure(.sessionIsNotValid))
+                print(error!.localizedDescription)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(.brokenResponse))
+                return
+            }
+            
+            switch response.statusCode {
+            case 200..<201:
+                guard let data = data else {
+                    completion(.failure(.NoDataRecieved))
+                    return
+                }
+                do {
+                    let users = try JSONDecoder().decode(Users.self, from: data)
+                    completion(.success(users.results))
+                } catch {
+                    completion(.failure(.unableToDecode))
+                }
+            default:
+                completion(.failure(.unexpectedStatusCode(response.statusCode)))
+            }
+        }
+    }
+    
+}
--- Axon_Assignement/Axon_Assignement/Network/Users.swift
+++ Axon_Assignement/Axon_Assignement/Network/Users.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-struct UserResponse {
+struct Users: Codable {
     var results = [User]()
 }
