d5f43069e2fb787c951221d7ea4b427a36bb50ac
Titanscull <ispetrovskyi@gmail.com>
2021-04-04 23:37:51 +0300

    DetailVC file added, UI outlets & image connected

---
+++ Axon_Assignement/Axon_Assignement/UsersListController/DetailVC/DetailVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailVC.swift
+//  Axon_Assignement
+//
+//  Created by Ievgen Petrovskiy on 04.04.2021.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var dobLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    
+}
